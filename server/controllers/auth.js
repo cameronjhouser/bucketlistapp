90828e589f305aa27dc7eab0d297d8b87dfce86b
Cameron Houser <cameronjhouser@gmail.com>
2017-02-20 15:50:35 -0500

    token add on

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,17 @@
 var User = require('../models/user');
+var jwt = require('jwt-simple');
+var config = require('../config');
+
+function createUserToken(user){
+	var timestamp = new Date().getTime();
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next){
 	// 1 
 	var email = req.body.email;
 	var password = req.body.password;
+	
 		if( !email || !password){
 			return res.status(418).send({error: 'You must provide email and pw.'});
 		}
@@ -12,24 +20,24 @@ exports.signup = function(req, res, next){
 		if(err) {
 			return next(err);
 		}//handle search error
-
 		if(existingUser){
 			// return res.status(418).send(err);
 			return res.status(418).send("Email is in use");
 
 		} // handles existing users
 
-	});
+	
 	 // 3
 	 var user = new User({
 	 	email: email,
 	 	password: password
 	 });
 
-	 // To save teh record to the DB 
+	 // To save the record to the DB 
 	 user.save(function(err){
 	 	if(err) { return next(err); }
 	 	// 4 Respond to request indicating the user was created 
-	 	res.json({success: true});
+	 	res.json({ token: createUserToken(user)});
 	 });
+	}); 
 }
