3a4bab8fd46713dbdb3f6633ba59a11ec7f14ff7
Cameron Houser <cameronjhouser@gmail.com>
2017-02-21 11:08:42 -0500

    Components Reinforcement

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import JSXIntro from './1_JSXIntro';
 import ComponentsIntro from './2_ComponentsIntro';
-// import PropsIntro from './3_PropsIntro';
+import PropsIntro from './3_PropsIntro';
 // import StateIntro from './4_StateIntro';
 // import ChildComponentsIntro from './5_ChildComponentsIntro';
 
@@ -13,6 +13,7 @@ import ComponentsIntro from './2_ComponentsIntro';
 					<h1>React Intro</h1>
 					<JSXIntro />
 					<ComponentsIntro />
+					<PropsIntro name="Michael Bolton" title="The Neverending Elevator Music" />
 				</div>
 				);
 		}
