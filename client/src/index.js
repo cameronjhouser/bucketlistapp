b09d2c9f5b5fac619cb0d4403ffed3e36530f68b
Cameron Houser <cameronjhouser@gmail.com>
2017-02-21 13:48:04 -0500

    NavBarHeader

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,8 +10,13 @@ import reducers from './reducers';
 var createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+	<Provider store={createStoreWithMiddleware(reducers)}>
+	          <Router history={browserHistory}>
+	              <Route path="/" component={App}>
+	              </Route>
+	          </Router>
+	</Provider>
+
+
   , document.querySelector('.container'));
 
