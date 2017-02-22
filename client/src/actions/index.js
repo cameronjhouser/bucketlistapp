faf62475d7a6eb4986dbdd9dc53217837c6a9478
Cameron Houser <cameronjhouser@gmail.com>
2017-02-21 22:31:47 -0500

    action discovery

---
+++ client/src/actions/index.js
@@ -0,0 +1,19 @@
+// Action Constant Names
+const SELECT_BAND = 'SELECT_BAND';
+{
+	type: SELECT_BAND,
+	payload:band
+}
+
+//This is our action
+// const ADD_BAND = 'ADD_BAND';
+// {
+// 	type: ADD_BAND,
+// 	payload:band
+// }
+
+// const DELETE_BAND = 'DELETE_BAND'
+// {
+// 	type: DELETE_BAND,
+// 	payload:band
+// }
